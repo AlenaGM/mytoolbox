01bc2855ac255a71094ef58ce44f8829851c99ed
Alena Guillaume <alena.guillaume4@gmail.com>
2024-03-05 12:53:01 +0100

    upd .env file

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cgjimtwnmnspqltboowq.supabase.co";
-//const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnamltdHdubW5zcHFsdGJvb3dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUwNjU5MDMsImV4cCI6MTk5MDY0MTkwM30.AFWFB6vviV-0PyQAYcWXcLNHs_OKhh5oPrvW4ha7ZXs";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
