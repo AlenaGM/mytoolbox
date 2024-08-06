31ba85247d6e5c776f38a22a22b89e579cd7ccd5
Alena Guillaume <alena.guillaume4@gmail.com>
2024-08-06 20:01:51 +0200

    upd category names

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,12 +1,12 @@
 const CATEGORIES = [
-  { name: "books", color: "#ef4444", label: "Books & Guides" },
-  { name: "ui/ux/anims", color: "#f97316", label: "UI/UX/Anims" },
-  { name: "html/css/js", color: "#eab308", label: "HTML, CSS, JS, TS" },
-  { name: "vue", color: "#16a34a", label: "Vue" },
+  { name: "guides", color: "#ef4444", label: "Guides & Blogs" },
+  { name: "html/css", color: "#f97316", label: "HTML, CSS" },
+  { name: "js/ts", color: "#eab308", label: "JS, TS" },
+  { name: "vue", color: "#16a34a", label: "Vue, Nuxt" },
   { name: "react", color: "#14b8a6", label: "React" },
-  { name: "blogs", color: "#3b82f6", label: "Blogs & People" },
-  { name: "career", color: "#8b5cf6", label: "Job-Search & Career" },
-  { name: "news", color: "#db2777", label: "IT-News & Releases" },
+  { name: "imgs", color: "#3b82f6", label: "Icons, Images, Anims" },
+  { name: "ui/ux", color: "#8b5cf6", label: "UI/UX/WCAG" },
+  { name: "npms", color: "#db2777", label: "NPMs & Libs" },
 ];
 
 export default CATEGORIES;
